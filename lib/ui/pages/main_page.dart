47e53a9f140fc12a6db20dcd1808d9c82da39f91
fathlaziss <71271714+fathlaziss@users.noreply.github.com>
2020-10-13 22:44:48 +0700

    [Muhamad Fathul Azis]
    add main page navigation bar

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -6,29 +6,148 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  int bottomNavBarIndex;
+  PageController pageController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    bottomNavBarIndex = 0;
+    pageController = PageController(initialPage: bottomNavBarIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Main Page"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            BlocBuilder<UserBloc, UserState>(
-                builder: (_, userState) => (userState is UserLoaded)
-                    ? Text(userState.user.name)
-                    : SizedBox()),
-            RaisedButton(
-              child: Text("Sign Out"),
-              onPressed: () {
-                AuthServices.signOut();
-              },
+      body: Stack(
+        children: <Widget>[
+          Container(
+            color: accentColor1,
+          ),
+          SafeArea(
+            child: Container(
+              color: Color(0xFFF6F7F9),
             ),
-          ],
-        ),
+          ),
+          PageView(
+            controller: pageController,
+            onPageChanged: (index) {
+              setState(() {
+                bottomNavBarIndex = index;
+              });
+            },
+            children: <Widget>[
+              Center(
+                child: Text("New Movies"),
+              ),
+              Center(
+                child: Text("My Tickets"),
+              )
+            ],
+          ),
+          createCustomBottomNavbar(),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 46,
+              width: 46,
+              margin: EdgeInsets.only(bottom: 42),
+              child: FloatingActionButton(
+                elevation: 0,
+                backgroundColor: accentColor2,
+                child: SizedBox(
+                  height: 26,
+                  width: 26,
+                  child: Icon(
+                    MdiIcons.walletPlus,
+                    color: Colors.black.withOpacity(0.54),
+                  ),
+                ),
+                onPressed: () {},
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
+
+  Widget createCustomBottomNavbar() => Align(
+        alignment: Alignment.bottomCenter,
+        child: ClipPath(
+          clipper: BottomNavBarClipper(),
+          child: Container(
+            height: 70,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20))),
+            child: BottomNavigationBar(
+              elevation: 0,
+              backgroundColor: Colors.transparent,
+              selectedItemColor: mainColor,
+              unselectedItemColor: Color(0xFFE5E5E5),
+              currentIndex: bottomNavBarIndex,
+              onTap: (index) {
+                setState(() {
+                  bottomNavBarIndex = index;
+                  pageController.jumpToPage(index);
+                });
+              },
+              items: [
+                BottomNavigationBarItem(
+                    title: Text(
+                      "New Movies",
+                      style: GoogleFonts.raleway(
+                          fontSize: 11, fontWeight: FontWeight.w500),
+                    ),
+                    icon: Container(
+                      margin: EdgeInsets.only(bottom: 6),
+                      height: 18,
+                      width: 24,
+                      child: Image.asset((bottomNavBarIndex == 0)
+                          ? "assets/ic_movie.png"
+                          : "assets/ic_movie_gray.png"),
+                    )),
+                BottomNavigationBarItem(
+                    title: Text(
+                      "MyTickets",
+                      style: GoogleFonts.raleway(
+                          fontSize: 11, fontWeight: FontWeight.w500),
+                    ),
+                    icon: Container(
+                      margin: EdgeInsets.only(bottom: 6),
+                      height: 18,
+                      width: 24,
+                      child: Image.asset((bottomNavBarIndex == 1)
+                          ? "assets/ic_ticket.png"
+                          : "assets/ic_ticket_grey.png"),
+                    ))
+              ],
+            ),
+          ),
+        ),
+      );
+}
+
+class BottomNavBarClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+
+    path.lineTo(size.width / 2 - 29, 0);
+    path.quadraticBezierTo(size.width / 2 - 29, 33.5, size.width / 2, 33.5);
+    path.quadraticBezierTo(size.width / 2 + 29, 33.5, size.width / 2 + 29, 0);
+    path.lineTo(size.width, 0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -6,6 +6,8 @@ import 'package:flutter_movie_tickets/bloc/blocs.dart';
 import 'package:flutter_movie_tickets/services/services.dart';
 import 'package:flutter_movie_tickets/shared/shared.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
